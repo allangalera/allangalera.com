11030f7a582a91c2df237cbbb36e7b83b404cbef
Allan Galera <allangalera@gmail.com>
2023-02-26 16:36:04 -0300

    setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+
+module.exports = {
+    extends: [
+      "plugin:astro/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    plugins: ["prettier", "@typescript-eslint"],
+    parser: '@typescript-eslint/parser',
+    rules: {
+      "@typescript-eslint/switch-exhaustiveness-check": "warn",
+      "prettier/prettier": "error"
+    },
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: __dirname,
+    },
+    overrides: [
+      {
+        files: ["*.astro"],
+        parser: "astro-eslint-parser",
+        parserOptions: {
+          parser: "@typescript-eslint/parser",
+          extraFileExtensions: [".astro"],
+        },
+        rules: {
+          "prettier/prettier": "off"
+        },
+      },
+    ],
+  }
