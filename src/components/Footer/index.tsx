4ca14f23e18ac655257dd80486d20ccb6fdd6f3d
Allan Galera <allangalera@gmail.com>
2023-02-26 19:18:29 -0300

    lighter border on light mode

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { FiGithub, FiLinkedin } from "solid-icons/fi";
 
 export const Footer = () => {
   return (
-    <footer class="flex gap-2 justify-center items-center p-2 bg-zinc-50 dark:bg-zinc-900 border-t-zinc-700 border-t">
+    <footer class="flex gap-2 justify-center items-center p-2 bg-zinc-50 dark:bg-zinc-900 border-t border-t-zinc-300 dark:border-t-zinc-700">
       <a
         class="text-zinc-900 dark:text-zinc-50 text-3xl rounded-lg hover:bg-zinc-100 dark:hover:bg-zinc-800 p-2"
         href="https://github.com/allangalera/"
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { ThemeDropdown } from "@/components/ThemeDropdown";
 
 export const Header = () => {
   return (
-    <header class="flex justify-between items-center sticky top-0 p-2 bg-zinc-50 dark:bg-zinc-900 border-b-zinc-700 border-b">
+    <header class="flex justify-between items-center sticky top-0 p-2 bg-zinc-50 dark:bg-zinc-900 border-b border-b-zinc-300 dark:border-b-zinc-700">
       <a
         class="text-zinc-900 dark:text-zinc-50 text-3xl rounded-lg hover:bg-zinc-100 dark:hover:bg-zinc-800 p-2 leading-tight"
         href="/"
