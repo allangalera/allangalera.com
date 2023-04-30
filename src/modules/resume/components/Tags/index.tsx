a59ded4d18811d36977c90b8f2f7cf665fbf1959
Allan Galera <allangalera@gmail.com>
2023-04-30 19:07:20 -0300

    feat: Improve tags readability

--- src/modules/resume/components/Tags/index.tsx
+++ src/modules/resume/components/Tags/index.tsx
@@ -8,7 +8,7 @@ type TagProps = {
 export const Tags: Component<TagProps> = (props) => {
   return (
     <Show when={props.tag.trim().length > 0}>
-      <p class="text-sm text-emerald-600 dark:text-emerald-300">
+      <p class="text-sm text-emerald-800 dark:text-emerald-400">
         #{pascalCase(props.tag.trim())}
       </p>
     </Show>
