31e82237b6ade3c42004fc445295cb9044e8816c
Allan Galera <allangalera@gmail.com>
2024-04-30 21:23:00 +0200

    feat: update astro

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,18 @@ import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 import aws from 'astro-sst';
+import rehypeExternalLinks from 'rehype-external-links';
 import { remarkReadingTime } from './src/remark/remark-reading-time';
+
+const rehypeExternalLinksConfig = {
+  target: '_blank',
+  rel: ['nofollow', 'noopener'],
+} satisfies Parameters<typeof rehypeExternalLinks>[0];
 
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, rehypeExternalLinksConfig]],
   },
   integrations: [
     mdx({
