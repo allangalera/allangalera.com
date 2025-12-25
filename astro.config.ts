0e4aace84e09cd58ad8a5ae086b114176e975f96
Allan Galera <allangalera@gmail.com>
2025-12-25 16:53:25 +0100

    feat: Remove markdown customization

--- astro.config.ts
+++ astro.config.ts
@@ -1,33 +1,22 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import solidJs from '@astrojs/solid-js';
-import sitemap from '@astrojs/sitemap';
-import aws from 'astro-sst';
-import rehypeExternalLinks from 'rehype-external-links';
-import { remarkReadingTime } from './src/remark/remark-reading-time';
-import tailwindcss from '@tailwindcss/vite';
-
-const rehypeExternalLinksConfig = {
-  target: '_blank',
-  rel: ['nofollow', 'noopener'],
-} satisfies Parameters<typeof rehypeExternalLinks>[0];
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import solidJs from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
+import aws from "astro-sst";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [[rehypeExternalLinks, rehypeExternalLinksConfig]],
-  },
   integrations: [
     mdx({
       shikiConfig: {
-        theme: 'css-variables',
+        theme: "css-variables",
       },
     }),
     solidJs(),
     sitemap(),
   ],
-  site: 'https://allangalera.com',
-  output: 'server',
+  site: "https://allangalera.com",
+  output: "server",
   adapter: aws(),
   vite: {
     plugins: [tailwindcss()],
